9fe6db3e29be2cd3cd8e706393517b2d0be72210
DhavalGhosT <dhaval.umarale@gmail.com>
2019-08-25 20:11:19 +0530

    added gitignore

--- etl/settings.py
+++ etl/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_page',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
